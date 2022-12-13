5cfc7d2e169750fb81c11dcaaccf81a7acca8c39
Erika Heidi <erika@erikaheidi.com>
2022-12-13 19:05:29 +0100

    Created custom command to build docs
    Signed-off-by: Erika Heidi <erika@erikaheidi.com>

---
+++ app/Command/Build/MelangeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Command\Build;
+
+use Minicli\Command\CommandController;
+
+class MelangeController extends CommandController
+{
+    public function handle(): void
+    {
+        $source = __DIR__ . '/../../../workdir/yaml/pipelines';
+        $output = __DIR__ . '/../../../workdir/markdown/melange-pipelines';
+        $yamldocs = __DIR__ . '/../../../vendor/erikaheidi/yamldocs/bin/yamldocs';
+
+        foreach (glob($source . '/*') as $path) {
+            if (is_dir($path) AND basename($path) !== "_meta") {
+                $outputDir = $output . '/' . basename($path);
+                if (!is_dir($outputDir)) {
+                    mkdir($outputDir);
+                }
+
+                echo shell_exec("$yamldocs build docs source=$path output=$outputDir builder=melange-pipeline");
+            }
+        }
+        echo shell_exec("$yamldocs build docs source=$source output=$output");
+    }
+}
