fa03acfcb419de56fd4dfdf6ca7274c0a6cc95c3
Erika Heidi <erika@erikaheidi.com>
2023-06-26 20:11:27 +0200

    Adding support to importing json tags file

--- app/Command/Cache/RefreshController.php
+++ app/Command/Cache/RefreshController.php
@@ -2,6 +2,7 @@
 
 namespace App\Command\Cache;
 
+use App\Service\CacheService;
 use App\Service\ImageDiscoveryService;
 use Minicli\Command\CommandController;
 use Minicli\Exception\CommandNotFoundException;
@@ -19,9 +20,22 @@ class RefreshController extends CommandController
         /** @var ImageDiscoveryService $imagesDiscovery */
         $imagesDiscovery = $this->getApp()->imageDiscovery;
         $this->info("Warming up image tags cache...");
+        if ($this->hasParam('import')) {
+            $this->info("Importing cache from existing JSON...");
+            $file = $this->getParam('import');
+            if (!is_file($file)) {
+                throw new \Exception("Import file $file not found.");
+            }
+
+            /** @var ImageDiscoveryService $images */
+            $images = $this->getApp()->imageDiscovery;
+            $cache = $images->cache->getCache(ImageDiscoveryService::$CACHE_CHAINCTL);
+            $cache->save(file_get_contents($file), ImageDiscoveryService::$CHAINCTL_CACHE_TAGS);
+
+            $this->success("Cache updated from input json.");
+            return;
+        }
         $imagesDiscovery->getImagesTags(false);
-        $this->info("Warming up image repos cache...");
-        $imagesDiscovery->getImagesRepos(false);
 
         $this->success("Cache updated.");
     }
